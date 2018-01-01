8e911369eb310d5e0e779113d0423189273e574f
Markus Riegel <riegel.markus@googlemail.com>
2018-01-01 15:21:32 +0100

    🛶 Refactor app for options and new i18n

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,44 @@
 import { Request, Response } from 'express'
-import { DialogflowApp } from 'actions-on-google'
-import { getDict } from './i18n/resolver'
+import { I18NDialogflowApp } from './i18n/I18NDialogflowApp'
 
-import * as AllSchedulesAction from './actions/AllSchedulesAction'
-import * as EtaForRuleAction from './actions/EtaForRuleAction'
-import * as MerchandiseAction from './actions/MerchandiseAction'
-import * as SalmonRunAction from './actions/SalmonRunAction'
-import * as SchedulesAction from './actions/SchedulesAction'
+import { OptionAction } from './action/OptionAction'
+import * as allSchedulesAction from './action/AllSchedulesAction'
+import * as etaForRuleAction from './action/EtaForRuleAction'
+import * as merchandiseAction from './action/MerchandiseAction'
+import * as salmonRunAction from './action/SalmonRunAction'
+import * as schedulesAction from './action/SchedulesAction'
+
+interface Option {
+    name: string,
+    handler: (app: I18NDialogflowApp, optionKeyParts: string[]) => void
+}
+
+interface Action {
+    name: string,
+    handler: (app: I18NDialogflowApp) => void
+}
 
 export function createDialogflowApp(request: Request, response: Response) {
-    const dialogflowApp = new DialogflowApp({request, response})
-    dialogflowApp['lang'] = request.body['lang']
+    const options: Option[] = [
 
-    interface Action {
-        name: string,
-        handler: (app: DialogflowApp) => void
-    }
-    const actions: Action[] = [
-        AllSchedulesAction,
-        EtaForRuleAction,
-        MerchandiseAction,
-        SalmonRunAction,
-        SchedulesAction
     ]
 
-    const actionMap = new Map()
-    actions.forEach(action => actionMap.set(action.name, action.handler))
-    dialogflowApp.handleRequest(actionMap)
+    const optionsAction = new OptionAction(options.reduce(
+        (map, option) => map.set(option.name, option.handler),
+        new Map<string, Option['handler']>()))
+
+    const actions: Action[] = [
+        allSchedulesAction,
+        etaForRuleAction,
+        merchandiseAction,
+        salmonRunAction,
+        schedulesAction,
+        optionsAction
+    ]
+
+    new I18NDialogflowApp({ request, response })
+        .handleRequest(actions.reduce(
+            (map, action) => map.set(action.name, action.handler), 
+            new Map<string, Action['handler']>()))
 }
   
